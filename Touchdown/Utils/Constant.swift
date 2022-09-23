9836a4a0d77db349b6d1480300331019bdd43945
Rafael Batista <maranata_rafa@hotmail.com>
2022-09-22 22:14:41 -0400

    add resources and create footerView

---
+++ Touchdown/Utils/Constant.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+//MARK: Data
+//MARK: Color
+let colors = (
+    background: Color("ColorBackground"),
+    gray: Color(UIColor.systemGray4)
+)
+
+//MARK: Layout
+//MARK: UX
+//MARK: API
+//MARK: Font
+//MARK: String
+//MARK: Misc
