0d91ce31370003567d3861933fda1cba278cabf8
Rafael Batista <maranata_rafa@hotmail.com>
2022-09-25 22:10:18 -0400

    create title view

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -20,6 +20,8 @@ struct ContentView: View {
 
                         CategoryGridView()
 
+                        TitleView(title: "Helmet")
+
                         FooterView()
                             .padding(.horizontal)
                     }
